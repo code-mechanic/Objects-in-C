3335afda699b0317c39f749cad8351f4006fa376
mohan <hashincludefun.h@gmail.com>
2020-06-10 08:20:27 +0530

    Basic constructor and function call from class is performed

---
+++ execise/Prog1_basic implementation.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int sub(int a, int b)
+{
+    return a-b;
+}
+
+typedef int (*add_ft)(int, int);
+typedef int (*sub_ft)(int, int);
+
+typedef struct
+{
+    add_ft add;
+    sub_ft sub;
+}Arithematic;
+
+void Arith_ctor(Arithematic *current)
+{
+    (*current).add = add;
+    (*current).sub = sub;
+}
+
+void main()
+{
+    Arithematic Arith;
+    Arith_ctor(&Arith);
+    int addition = Arith.add(5,2);
+    int subtraction = Arith.sub(5,2);
+    printf("Results : %d and %d",addition,subtraction);
+}
